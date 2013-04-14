b2e2acaa72df824ec6b42d8fdbb3f070541ce420
jvmakine <jvmakine@gmail.com>
2013-04-14 23:28:43 +0300

    Moved MD% to utils namespace

--- src/accounting/operations.clj
+++ src/accounting/operations.clj
@@ -5,25 +5,16 @@
     (sandbar core stateful-session))
   (:require [accounting.urls :as urls])
   (:require [accounting.dao.user :as user])
-  (:import (java.security MessageDigest)))
-
-(defn md5 [token]
-  (let [hash-bytes
-        (doto (java.security.MessageDigest/getInstance "MD5")
-          (.reset)
-          (.update (.getBytes token)))]
-    (.toString
-      (new java.math.BigInteger 1 (.digest hash-bytes)) 16)))
+  (:require [accounting.utils :as utils]))
 
 (defn login [username password]
   (let [user (user/get-by-username username)]
-    (if (= (md5 password) (:password user))
+    (if (= (utils/md5 password) (:password user))
       (do
         (session-put! "username" username)
         (session-put! "roles" #{:user})
         (redirect urls/root))
-      (redirect urls/login))
-    ))
+      (redirect urls/login))))
 
 (defn logout []
   (do
@@ -33,5 +24,5 @@
 
 (defn signup [username password]
   (do
-    (user/new username (md5 password))
+    (user/new username (utils/md5 password))
     (redirect urls/root)))
