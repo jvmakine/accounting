2ffb9b582ac1661f342a339f9675beac144ea8df
jvmakine <jvmakine@gmail.com>
2013-04-29 19:33:55 +0300

    Listing of account names from the server

--- resources/public/js/accounting.js
+++ resources/public/js/accounting.js
@@ -1,8 +1,32 @@
 "use strict";
 var accounting = (function() {
-  var publicInterace = {};
   
+  var publicInterface = {};
   
+  var Accounts = newCollection('/rest/account');
+  
+  var AccountModel = Backbone.Model.extend({
+    urlRoot: '/rest/account',
+    defaults: {}
+  });
+  
+  function newCollection(url) {
+    var result = new Backbone.Collection;
+    result.url = url;
+    return result;
+  }
+  
+  function collectionToJSON(collection) {
+    return _.map(collection.models, function(model) { return model.toJSON(); } );
+  }
+  
+  publicInterface.listAccounts = function() {
+    return collectionToJSON(Accounts);
+  }
+  
+  publicInterface.init = function() {
+    Accounts.fetch();
+  }
   
   return publicInterface;
 }());
