dd1c260ba483240deef5fc717fa5796c54447107
jvmakine <jvmakine@gmail.com>
2013-06-18 13:20:27 +0300

    Redirect to login after session has expired

---
+++ resources/public/js/backbone-mod.js
@@ -0,0 +1,18 @@
+$(function() {
+  
+  var oldSync = Backbone.sync;
+  
+  Backbone.sync = function(method, model, opts) {
+    var oldError = opts.error;
+    oldSync(method, model, _.extend(opts, {
+      error: function(xhr, text_status, error_thrown) {
+        if(xhr.status == 200) { //Session expired, redirect to login
+          window.location.replace("/login");
+        } else if(oldError) {
+          oldError(xhr, text_status, error_thrown);
+        }
+      }
+    }));
+  }
+  
+});
